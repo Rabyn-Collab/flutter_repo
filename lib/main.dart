a8c38460b5a2dea84009027ccaa74c0fd3456903
Rabyn <rabyn900@gmail.com>
2022-06-05 15:51:33 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sample/provider/counter_provider.dart';
@@ -5,8 +8,43 @@ import 'package:flutter_sample/view/home_page.dart';
 import 'package:get/get.dart';
 
 
+class Post{
 
-void main (){
+  final int id;
+  final String title;
+  final String body;
+
+  Post({required this.body, required this.id, required this.title});
+
+  factory Post.fromJson(Map<String, dynamic> json){
+    return  Post(
+        body: json['body'],
+        id: json['id'],
+        title: json['title']
+    );
+  }
+
+
+}
+
+
+
+Future<List<Post>>  getData() async {
+
+  final dio = Dio();
+  try{
+     final response = await dio.get('https://jsonplaceholder.typicode.com/posts');
+     final data = (response.data as List).map((e) => Post.fromJson(e)).toList();
+     return data;
+  }on DioError catch (err){
+    return [];
+  }
+}
+
+
+void main () async{
+  final m = await getData();
+  print(m);
   runApp(ProviderScope(child: Home()));
 }
 
